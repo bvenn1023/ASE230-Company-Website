48b9d16dc0ed41f5d856ae7ca29c7ce24dbfc393
Hunterperry21 <143469730+Hunterperry21@users.noreply.github.com>
2023-09-18 22:12:55 -0400

    Update csv.php

--- csv.php
+++ csv.php
@@ -6,9 +6,9 @@ header('Content-Type: text/csv; charset=utf-8');
 // Set the response header to specify that the file should be downloaded as an attachment
 header('Content-Disposition: attachment; filename=data.csv');
 
-Data array
+
 $data = array(
-    array(
+'Awards' => array(
         'Company Name' => 'Orion Aerospace Dynamics',
         'Overview' => 'Orion Aerospace Dynamics, founded in 2021, is a game-changing aerospace company headquartered atop the serene peaks of Denver, Colorado. Championing the motto "The Sky is Not the Limit," Orion has been redefining space exploration and aviation with advanced propulsion systems, space habitats, and aerial vehicles that seem straight out of science fiction.',
         'Mission Statement' => 'To push the boundaries of human reach, making the vast expanse of space a familiar terrain and ensuring Earth\'s skies are traveled sustainably.',
@@ -29,7 +29,7 @@ $data = array(
         'Awards' => '2023: Secured "Aerospace Company of the Decade" title by Aviation Weekly. 2022: Successfully launched three Galactic CruiserTM missions to Mars, paving the way for potential human settlements. 2021: SkySailor DronesTM adopted by leading ecological researchers for environmental monitoring without carbon footprints. 2021: Partnership with NASA and SpaceX for the development of next-generation propulsion technologies.',
     ),
     array(
-        'Team' => 'Capt. Helena Vance - Founder & CEO, Dr. Hiroshi Nakamura - CTO, Leo Rodriguez - Chief of Design, Ava O\'Connell - VP of Operations, Dr. Imani Okoro - Head of AeroAcademyTM',
+       'Team' => array( 'Capt. Helena Vance - Founder & CEO, Dr. Hiroshi Nakamura - CTO, Leo Rodriguez - Chief of Design, Ava O\'Connell - VP of Operations, Dr. Imani Okoro - Head of AeroAcademyTM',
     ),
 );
 
