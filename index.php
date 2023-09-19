d5c6fc5fa8ab01a4143bf56ab0c627ecbad35833
Will Cuthbert <cuthbertw1@nku.edu>
2023-09-18 21:10:35 -0400

    modified:   data/overview.txt 	modified:   index.php

--- index.php
+++ index.php
@@ -57,9 +57,7 @@ require 'lib/txt.php';
                     <li class="nav-item">
                         <a href="#features" class="nav-link">Features</a>
                     </li>
-                    <li class="nav-item">
-                        <a href="#pricing" class="nav-link">Pricing</a>
-                    </li>
+
                     <li class="nav-item">
                         <a href="#team" class="nav-link">Team</a>
                     </li>
@@ -85,10 +83,10 @@ require 'lib/txt.php';
                     <div class="text-center">
                         <span class="badge badge-soft-primary mb-4">Orion Aerospace</span>
                         <h1 class="font-weight-semibold mb-4 hero-3-title">Mission Statement</h1>
-                        <p class="mb-5 text-muted subtitle w-75 mx-auto">Mission</p>
+                        <p class="mb-5 text-muted subtitle w-75 mx-auto"><?php text('data/mission.txt')?></p>
 
                         <div>
-                            <button type="button" class="btn btn-primary rounded-pill me-2">Sign up for free</button>
+                            <button type="button" class="btn btn-primary rounded-pill me-2">Work With Us</button>
                             <button type="button" class="btn btn-light rounded-pill me-2" data-bs-toggle="modal" data-bs-target="#watchvideomodal">Play video <i class="ms-1 icon-sm align-middle" data-feather="play-circle"></i></button>
                         </div>
 
@@ -224,7 +222,7 @@ require 'lib/txt.php';
                 <div class="col-lg-8">
                     <div class="text-center">
                         <h1 class="text-white mb-4">Who We Are</h1>
-                        <p class="text-white mb-5 font-size-16">Overview</p>
+                        <p class="text-white mb-5 font-size-16"><?php text('data/overview.txt')?></p>
                         <a href="#" class="btn btn-lg btn-light">Ask for Demonstration</a>
                     </div>
                 </div>
@@ -236,167 +234,7 @@ require 'lib/txt.php';
     </section>
     <!-- Cta end -->
 
-    <!-- Pricing start -->
-    <section class="section" id="pricing">
-        <div class="container">
-            <div class="row justify-content-center mb-5">
-                <div class="col-lg-7 text-center">
-                    <h2 class="fw-bold">Pricing Plan</h2>
-                    <p class="text-muted">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem ab illo inventore.</p>
-                </div>
-            </div>
-            <!-- end row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="text-center mb-4 pricing-tab">
-                        <ul class="nav nav-pills rounded-pill justify-content-center d-inline-block shadow-sm" id="pricingpills-tab" role="tablist">
-                            <li class="nav-item d-inline-block">
-                                <a class="nav-link rounded-pill active" id="pills-monthly-tab" data-bs-toggle="pill" href="#pills-monthly" role="tab" aria-controls="pills-monthly" aria-selected="true">Monthly</a>
-                            </li>
-                            <li class="nav-item d-inline-block">
-                                <a class="nav-link rounded-pill" id="pills-yearly-tab" data-bs-toggle="pill" href="#pills-yearly" role="tab" aria-controls="pills-yearly" aria-selected="false">Yearly</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="tab-content" id="pricingpills-tabContent">
-                        <div class="tab-pane fade show active" id="pills-monthly" role="tabpanel" aria-labelledby="pills-monthly-tab">
-                            <div class="row">
-                                <div class="col-lg-4">
-                                    <div class="card plan-card mt-4 rounded text-center border-0 shadow overflow-hidden">
-                                        <div class="card-body px-4 py-5">
-                                            <!-- <div class="icon-mono avatar-md bg-soft-primary text-primary rounded mx-auto mb-5"><i class="icon-lg" data-feather="circle"></i></div> -->
-                                            <div class="icon-mono avatar-md bg-soft-primary rounded mx-auto mb-5 p-3">
-                                                <img src="images/pricing/1.png" alt="" class="img-fluid d-block mx-auto" />
-                                            </div>
-                                            <h4 class="text-uppercase mb-4 pb-1">Basic</h4>
-                                            <p class="text-muted">Onlinespace: <span class="fw-bold">50MB</span></p>
-                                            <p class="text-muted">Support: <span class="fw-bold">No</span></p>
-                                            <p class="text-muted mb-4 pb-1">Domain 1</p>
-                                            <p class="text-muted font-size-14 mb-1">All Extension Included</p>
-                                            <p class="font-size-16 font-weight-semibold mb-4 price-tag">$9.00 / Month</p>
-                                            <a href="javascript: void(0);" class="btn btn-soft-primary">Buy Now</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- end col -->
-                                <div class="col-lg-4">
-                                    <div class="card plan-card mt-4 rounded text-center border-0 shadow overflow-hidden">
-                                        <div class="card-body px-4 py-5">
-                                            <span class="badge badge-primary pricing-badge shadow-lg">Most Popular</span>
-                                            <!-- <div class="icon-mono avatar-md bg-soft-primary text-primary rounded mx-auto mb-5"><i class="icon-lg" data-feather="square"></i></div> -->
-                                            <div class="icon-mono avatar-md bg-soft-primary rounded mx-auto mb-5 p-3">
-                                                <img src="images/pricing/2.png" alt="" class="img-fluid d-block mx-auto" />
-                                            </div>
-                                            <h4 class="text-uppercase mb-4 pb-1">Standard</h4>
-                                            <p class="text-muted">Onlinespace: <span class="fw-bold">100MB</span></p>
-                                            <p class="text-muted">Support: <span class="fw-bold">Yes</span></p>
-                                            <p class="text-muted mb-4 pb-1">Domain 1</p>
-                                            <p class="text-muted font-size-14 mb-1">All Extension Included</p>
-                                            <p class="font-size-16 font-weight-semibold mb-4 price-tag">$39.00 / Month</p>
-                                            <a href="javascript: void(0);" class="btn btn-primary">Buy Now</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- end col -->
-                                <div class="col-lg-4">
-                                    <div class="card plan-card mt-4 rounded text-center border-0 shadow overflow-hidden">
-                                        <div class="card-body px-4 py-5">
-                                            <!-- <div class="icon-mono avatar-md bg-soft-primary text-primary rounded mx-auto mb-5"><i class="icon-lg" data-feather="triangle"></i></div> -->
-                                            <div class="icon-mono avatar-md bg-soft-primary rounded mx-auto mb-5 p-3">
-                                                <img src="images/pricing/3.png" alt="" class="img-fluid d-block mx-auto" />
-                                            </div>
-                                            <h4 class="text-uppercase mb-4 pb-1">Premium</h4>
-                                            <p class="text-muted">Onlinespace: <span class="fw-bold">200MB</span></p>
-                                            <p class="text-muted">Support: <span class="fw-bold">No</span></p>
-                                            <p class="text-muted mb-4 pb-1">Domain 1</p>
-                                            <p class="text-muted font-size-14 mb-1">All Extension Included</p>
-                                            <p class="font-size-16 font-weight-semibold mb-4 price-tag">$79.00 / Month</p>
-                                            <a href="javascript: void(0);" class="btn btn-soft-primary">Buy Now</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- end col -->
-                            </div>
-                            <!-- end row -->
-                        </div>
-                        <!-- end monthly tab pane -->
-
-                        <div class="tab-pane fade" id="pills-yearly" role="tabpanel" aria-labelledby="pills-yearly-tab">
-                            <div class="row">
-                                <div class="col-lg-4">
-                                    <div class="card plan-card mt-4 rounded text-center border-0 shadow overflow-hidden">
-                                        <div class="card-body px-4 py-5">
-                                            <!-- <div class="icon-mono avatar-md bg-soft-primary text-primary rounded mx-auto mb-5"><i class="icon-lg" data-feather="circle"></i></div> -->
-                                            <div class="icon-mono avatar-md bg-soft-primary rounded mx-auto mb-5 p-3">
-                                                <img src="images/pricing/1.png" alt="" class="img-fluid d-block mx-auto" />
-                                            </div>
-                                            <h4 class="text-uppercase mb-4 pb-1">Basic</h4>
-                                            <p class="text-muted">Onlinespace: <span class="fw-bold">50MB</span></p>
-                                            <p class="text-muted">Support: <span class="fw-bold">No</span></p>
-                                            <p class="text-muted mb-4 pb-1">Domain 1</p>
-                                            <p class="text-muted font-size-14 mb-1">All Extension Included</p>
-                                            <p class="font-size-16 font-weight-semibold mb-4 price-tag">$29.00 / Year</p>
-                                            <a href="javascript: void(0);" class="btn btn-soft-primary">Buy Now</a>
-                                        </div>
-                                        <!-- end cardbody -->
-                                    </div>
-                                    <!-- end card -->
-                                </div>
-                                <!-- end col -->
-                                <div class="col-lg-4">
-                                    <div class="card plan-card mt-4 rounded text-center border-0 shadow overflow-hidden">
-                                        <div class="card-body px-4 py-5">
-                                            <!-- <div class="icon-mono avatar-md bg-soft-primary text-primary rounded mx-auto mb-5"><i class="icon-lg" data-feather="square"></i></div> -->
-                                            <div class="icon-mono avatar-md bg-soft-primary rounded mx-auto mb-5 p-3">
-                                                <img src="images/pricing/2.png" alt="" class="img-fluid d-block mx-auto" />
-                                            </div>
-                                            <h4 class="text-uppercase mb-4 pb-1">Standard</h4>
-                                            <p class="text-muted">Onlinespace: <span class="fw-bold">100MB</span></p>
-                                            <p class="text-muted">Support: <span class="fw-bold">Yes</span></p>
-                                            <p class="text-muted mb-4 pb-1">Domain 1</p>
-                                            <p class="text-muted font-size-14 mb-1">All Extension Included</p>
-                                            <p class="font-size-16 font-weight-semibold mb-4 price-tag">$49.00 / Year</p>
-                                            <a href="javascript: void(0);" class="btn btn-soft-primary">Buy Now</a>
-                                        </div>
-                                        <!-- end cardbody -->
-                                    </div>
-                                    <!-- end card -->
-                                </div>
-                                <!-- end col -->
-                                <div class="col-lg-4">
-                                    <div class="card plan-card mt-4 rounded text-center border-0 shadow overflow-hidden">
-                                        <div class="card-body px-4 py-5">
-                                            <span class="badge badge-primary pricing-badge shadow-lg">Most Popular</span>
-                                            <!-- <div class="icon-mono avatar-md bg-soft-primary text-primary rounded mx-auto mb-5"><i class="icon-lg" data-feather="triangle"></i></div> -->
-                                            <div class="icon-mono avatar-md bg-soft-primary rounded mx-auto mb-5 p-3">
-                                                <img src="images/pricing/3.png" alt="" class="img-fluid d-block mx-auto" />
-                                            </div>
-                                            <h4 class="text-uppercase mb-4 pb-1">Premium</h4>
-                                            <p class="text-muted">Onlinespace: <span class="fw-bold">200MB</span></p>
-                                            <p class="text-muted">Support: <span class="fw-bold">No</span></p>
-                                            <p class="text-muted mb-4 pb-1">Domain 1</p>
-                                            <p class="text-muted font-size-14 mb-1">All Extension Included</p>
-                                            <p class="font-size-16 font-weight-semibold mb-4 price-tag">$99.00 / Year</p>
-                                            <a href="javascript: void(0);" class="btn btn-soft-primary">Buy Now</a>
-                                        </div>
-                                        <!-- end cardbody -->
-                                    </div>
-                                    <!-- end card -->
-                                </div>
-                                <!-- end col -->
-                            </div>
-                        </div>
-                        <!-- end yearly tab pane -->
-                    </div>
-                    <!-- end tab content -->
-                </div>
-                <!-- end col -->
-            </div>
-            <!-- end row -->
-        </div>
-        <!-- end container -->
-    </section>
-    <!-- Pricing end -->
+   
 
     <!-- Team start -->
     <section class="section bg-light" id="team">
