e1e3d490645e679e4c621f8002cfed5bc9186c95
Hunterperry21 <143469730+Hunterperry21@users.noreply.github.com>
2023-09-18 22:41:37 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'lib/convertJson.php';
 require 'lib/txt.php';
 require 'lib/csv.php';
