9d1916f37c6f233d2cd23f54397faf77e8de78c5
bvenn1023 <144082597+bvenn1023@users.noreply.github.com>
2023-09-28 12:59:56 -0400

    Update csv.php

--- lib/csv.php
+++ lib/csv.php
@@ -1,54 +1,39 @@
-
-
 <?php
+// Function to print a team member's description by name
+function printcsv($name)
+{
+    // Specify the absolute path to the CSV file
+    $csvFile = __DIR__ . '/info.csv';
 
-// Set the content type to CSV
-header('Content-Type: text/csv; charset=utf-8');
+    // Check if the CSV file exists
+    if (file_exists($csvFile)) {
+        // Read the CSV file into an array
+        $csvData = array_map('str_getcsv', file($csvFile));
 
-// Set the response header to specify that the file should be downloaded as an attachment
-header('Content-Disposition: attachment; filename=data.csv');
+        // Extract headers (first row)
+        $headers = array_shift($csvData);
 
-Data array
-$data = array(
-    array(
-        'Company Name' => 'Orion Aerospace Dynamics',
-        'Overview' => 'Orion Aerospace Dynamics, founded in 2021, is a game-changing aerospace company headquartered atop the serene peaks of Denver, Colorado. Championing the motto "The Sky is Not the Limit," Orion has been redefining space exploration and aviation with advanced propulsion systems, space habitats, and aerial vehicles that seem straight out of science fiction.',
-        'Mission Statement' => 'To push the boundaries of human reach, making the vast expanse of space a familiar terrain and ensuring Earth\'s skies are traveled sustainably.',
-    ),
-    array(
-        'Key Products & Services' => 'Galactic CruiserTM: An innovative spacecraft designed for deep space exploration, equipped with advanced life support systems and AI-driven navigation. Applications: Stellar Drive Propulsion, Holodeck Entertainment',
-    ),
-    array(
-        'Key Products & Services' => 'SkySailor DronesTM: Environmentally-friendly drones powered by solar and wind energy, suitable for both recreational and professional use. Applications: Eco-Cam Tech, Infinite Flight Mode',
-    ),
-    array(
-        'Key Products & Services' => 'Nebula StationsTM: Modular space habitats intended for research, tourism, and potential future colonization efforts. Applications: Bio-Dome Agriculture, Gravity Pods',
-    ),
-    array(
-        'Key Products & Services' => 'AeroAcademyTM: An immersive training institution offering programs in aerospace engineering, astronaut training, and space tourism orientation. Applications: Virtual Spacewalks, Intergalactic Culture Modules',
-    ),
-    array(
-        'Awards' => '2023: Secured "Aerospace Company of the Decade" title by Aviation Weekly. 2022: Successfully launched three Galactic CruiserTM missions to Mars, paving the way for potential human settlements. 2021: SkySailor DronesTM adopted by leading ecological researchers for environmental monitoring without carbon footprints. 2021: Partnership with NASA and SpaceX for the development of next-generation propulsion technologies.',
-    ),
-    array(
-        'Team' => 'Capt. Helena Vance - Founder & CEO, Dr. Hiroshi Nakamura - CTO, Leo Rodriguez - Chief of Design, Ava O\'Connell - VP of Operations, Dr. Imani Okoro - Head of AeroAcademyTM',
-    ),
-);
+        // Search for the team member by name in the CSV data
+        foreach ($csvData as $row) {
+            $rowData = array_combine($headers, $row);
 
-// CSV file name
-$filename = 'orion_aerospace.csv';
+            if ($rowData['Name'] === $name) {
+                // Print the team member's description
+                echo '<h2>Team Member:</h2>';
+                echo '<h4 class="mb-3 font-size-22">' . $rowData['Name'] . '</h4>';
+                echo '<p class="text-muted mb-0">Description: ' . $rowData['Description'] . '</p>';
+                return; // Exit the function once found
+            }
+        }
 
-// Open a file for writing
-$file = fopen($filename, 'w');
-
-// Loop through the data and write each row as a CSV line
-foreach ($data as $row) {
-    fputcsv($file, $row, ',', '"');
+        // If the name wasn't found
+        echo "Team member '$name' not found in data.";
+    } else {
+        echo "CSV file not found.";
+    }
 }
 
 
-fclose($file);
 
-echo "CSV file '$filename' has been created.";
 
-?>
+
