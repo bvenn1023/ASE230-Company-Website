0caccad4335bc3a37ca5dd74c4808c5f57dfeb7c
bvenn1023 <144082597+bvenn1023@users.noreply.github.com>
2023-09-29 21:47:10 -0400

    new file:   admin/awards/awards.php (#15)
    new file:   admin/contacts/contacts.php
    	new file:   admin/pages/delete.php
    	new file:   admin/pages/index.php
    	new file:   admin/pages/pages.php
    	new file:   admin/products/products.php
    	new file:   admin/team/team.php
    
    Co-authored-by: Will Cuthbert <cuthbertw1@nku.edu>

---
+++ admin/pages/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+</head>
+<body>
+	<h1>Welcome Admin</h1>
+	<h1>click a link below to make changes to the page</h1>
+	<h2>delete item</h2>
+	<a href="delete.php">delete item</a>
+
+
+
+
+
+</body>
+</html>
