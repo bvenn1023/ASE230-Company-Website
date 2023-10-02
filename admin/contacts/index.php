c8028d39fdcaa1b89256a8cb5343f8597f7afc01
Hunterperry21 <143469730+Hunterperry21@users.noreply.github.com>
2023-10-02 14:05:34 -0400

    Update index.php

--- admin/contacts/index.php
+++ admin/contacts/index.php
@@ -1,3 +1,23 @@
 <?php
 require_once('contacts.php');
 loadPage('contacts','Contact us');
+?>
+<?php
+$contactRequests = []; // Assuming an array of contact requests
+
+        
+foreach ($contactRequests as $contact) {
+
+
+    $id = $contact['id']; // Replace 'id' with your unique identifier
+
+    $name = $contact['name'];
+    $email = $contact['email'];
+    echo "<tr>";
+    echo "<td>$name</td>";
+    echo "<td>$email</td>";
+    echo "<td><a href='detail.php?id=$id'>Details</a></td>";
+    echo "</tr>";
+}
+?>
+    
