6ff081495b91111b759067bfc04dcf1797f9f64c
Hunterperry21 <143469730+Hunterperry21@users.noreply.github.com>
2023-10-02 14:01:21 -0400

    Update detail.php

--- admin/contacts/detail.php
+++ admin/contacts/detail.php
@@ -1,1 +1,21 @@
 
+    <?php
+
+    if (isset($_GET['id'])) {
+        $id = $_GET['id']; 
+    
+        $contactDetails = []; 
+        
+        if (isset($contactDetails[$id])) {
+            $contact = $contactDetails[$id];
+            echo "<p><strong>Name:</strong> {$contact['name']}</p>";
+            echo "<p><strong>Email:</strong> {$contact['email']}</p>";
+
+        } else {
+            echo "<p>Contact not found.</p>";
+        }
+    } else {
+        echo "<p>Invalid request.</p>";
+    }
+    ?>
+
