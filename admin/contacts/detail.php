10c45373314a5dfdf669d563a3236d26177eeb0a
Hunterperry21 <143469730+Hunterperry21@users.noreply.github.com>
2023-10-02 23:06:33 -0400

    Update detail.php

--- admin/contacts/detail.php
+++ admin/contacts/detail.php
@@ -10,6 +10,7 @@
             $contact = $contactDetails[$id];
             echo "<p><strong>Name:</strong> {$contact['name']}</p>";
             echo "<p><strong>Email:</strong> {$contact['email']}</p>";
+            echo "<p><strong>Phone:</strong> {$contact['phone']}</p>";
 
         } else {
             echo "<p>Contact not found.</p>";
