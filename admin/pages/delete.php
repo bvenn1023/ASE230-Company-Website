88ae83216d5fbac8fb22770d51121a4792a16427
Will Cuthbert <cuthbertw1@nku.edu>
2023-10-03 11:29:28 -0400

    modified:   pages/delete.php 	new file:   pages/edit.php

--- admin/pages/delete.php
+++ admin/pages/delete.php
@@ -1,38 +1,9 @@
 <?php require 'pages.php';
 
-?>
 
-
-<table>
-	<?php 
-	$lines=file('../../data/info.txt');
-	for($i=0;$i<count($lines);$i++){
-	?>
-	<tr>
-		<td>
-		<?php 
-		echo ($i.'. ');
-		readText('../../data/info.txt',$i);
-		?>
-		</td>
-	</tr>
-	<?php }?>
-
-
-
-
-</table>
-
-
-<form method="POST" action='delete.php'/>
-<p>Enter line to delete</p>
-<input type="text" name="delselection">
-</form>
-
-<?php 
-if (isset($_POST["delselection"])){
-	deleteText('../../data/info.txt',$_POST["delselection"]);
-	print_r($_POST);
+if (isset($_GET["name"])){
+	deleteText('../../data/info.txt',$_GET["name"]);
+	print_r($_GET["name"]);
 	//echo(readText('../../data/info.txt','1'));
 	header("Location: index.php");
 	exit;
